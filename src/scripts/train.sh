872dc55cc947374897a814803000c7e86d169eab
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-17 21:34:49 -0400

    a

--- src/scripts/train.sh
+++ src/scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 conda activate pytorch
 cd ./yolov7
-num_freeze_layers = 118
-batch_size = 16
-num_epochs = 10
-data_name = "VisDroneData-2019"
-weight_filename = "yolov7x_training.pt"
-cfg_filename = "yolov7x.yaml"
+num_freeze_layers=118
+batch_size=16
+num_epochs=10
+data_name="VisDroneData-2019"
+weight_filename="yolov7x_training.pt"
+cfg_filename="yolov7x.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
 if [! -f yolov7x_training.pt]
