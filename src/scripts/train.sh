866f645f5a30f2587aed465ea0e210bfe695fab5
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-17 20:06:19 -0400

    added train.sh

---
+++ src/scripts/train.sh
@@ -0,0 +1,3 @@
+conda activate pytorch
+cd ./MLOps4-Capstone/yolov7
+python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
