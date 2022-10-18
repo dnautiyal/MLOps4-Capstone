8aff59c5cfc4f30cde6ee2e97b26c2bfc921a51d
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-17 20:19:42 -0400

    fixed

--- src/scripts/train.sh
+++ src/scripts/train.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 conda activate pytorch
 cd ./yolov7
 python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
