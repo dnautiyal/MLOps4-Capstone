2452978ff81b7136c951cd35f38b1cd0499d8fef
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-18 09:57:26 -0400

    created train.config file

--- src/train/train.sh
+++ src/train/train.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # https://thelinuxcluster.com/2020/05/21/cant-execute-conda-activate-from-bash-script/
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pytorch
 cd ./yolov7
-num_freeze_layers=118
-batch_size=16
-num_epochs=10
-dataset_name="VisDroneData-2019"
-weights_filename="yolov7x_training.pt"
-cfg_filename="yolov7x.yaml"
+source train.config
+# num_freeze_layers=118
+# batch_size=16
+# num_epochs=10
+# dataset_name="VisDroneData-2019"
+# weights_filename="yolov7x_training.pt"
+# cfg_filename="yolov7x.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
 if [! -f $weights_filename]
