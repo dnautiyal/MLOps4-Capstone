49b74eba6184070fa29220cfe8d1bd6fa30d93d2
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-18 11:37:30 -0400

    x

--- src/train/train.sh
+++ src/train/train.sh
@@ -3,14 +3,15 @@
 # source ~/conda/etc/profile.d/conda.sh
 eval "$(conda shell.bash hook)"
 conda activate pytorch
-# source ./src/train/train.config
-cd ./yolov7
-num_freeze_layers=118
-num_epochs=1
-dataset_name="VisDroneData-2019"
-batch_size=16
-weights_filename="yolov7x_training.pt"
-cfg_filename="yolov7x.yaml"
+source ./src/train/train_config.sh
+# cd ./yolov7
+# num_freeze_layers=118
+# num_epochs=1
+# dataset_name="VisDroneData-2019"
+# dataset_name="VisDrone2019-DET-YOLOv7"
+# batch_size=16
+# weights_filename="yolov7x_training.pt"
+# cfg_filename="yolov7x.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
 if [! -f $weights_filename]
