8feb4fceee3f0c84dd6f0d9e48e37999bd93fdf5
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-21 15:19:27 -0400

    changed to yolov7-w6_training and yolo7-w6.yaml

--- src/train/train.sh
+++ src/train/train.sh
@@ -6,11 +6,11 @@ conda activate pytorch
 # source ./src/train/train_config.sh
 cd ./yolov7
 num_freeze_layers=118
-num_epochs=50
+num_epochs=5
 dataset_name="VisDrone2019-DET-YOLOv7"
 batch_size=16
-weights_filename="yolov7x_training.pt"
-cfg_filename="yolov7x.yaml"
+weights_filename="yolov7-w6_training.pt"
+cfg_filename="yolov7-w6.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
 if [! -f $weights_filename]
@@ -19,4 +19,4 @@ then
 else
     echo "$weights_filename file already exists"
 fi
-python3 train.py --rect --img-size 960 --freeze $num_freeze_layers --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./cfg/training/$cfg_filename
+python3 train.py --adam --img-size 960 960 --freeze $num_freeze_layers --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./cfg/training/$cfg_filename
