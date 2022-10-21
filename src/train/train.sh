36f4d58d4640fa2e3d6ab18da0b46efa9013b27c
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-21 15:25:39 -0400

    changed train.sh file

--- src/train/train.sh
+++ src/train/train.sh
@@ -13,10 +13,10 @@ weights_filename="yolov7-w6_training.pt"
 cfg_filename="yolov7-w6.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
-if [! -f $weights_filename]
+if [-f $weights_filename]
 then
-    wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/$weight_filename
-else
     echo "$weights_filename file already exists"
+else
+    wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/$weight_filename    
 fi
 python3 train.py --adam --img-size 960 960 --freeze $num_freeze_layers --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./cfg/training/$cfg_filename
