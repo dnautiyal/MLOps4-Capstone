1b182f114ae1d90f63be840113df7835dbe2e027
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-21 21:16:38 -0400

    a

--- src/train/train_yolov5.sh
+++ src/train/train_yolov5.sh
@@ -6,10 +6,10 @@ conda activate pytorch
 # source ./src/train/train_config.sh
 cd ./yolov5
 num_freeze_layers=118
-num_epochs=10
+num_epochs=5
 dataset_name="VisDrone2019-DET-YOLOv7"
 batch_size=16
-weights_filename="yolov5x-cls.pt"
+weights_filename="yolov5s.pt"
 cfg_filename="yolov5x.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
@@ -19,4 +19,4 @@ then
 else
     wget https://github.com/ultralytics/yolov5/releases/download/v6.2/$weights_filename
 fi
-python3 train.py --img-size 960 --rect --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./models/$cfg_filename
+python3 train.py --img-size 960 --rect --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename
