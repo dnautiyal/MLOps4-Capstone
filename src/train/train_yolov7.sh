6787575d751193b2788f0bb3eac92e0a43ab99d0
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-25 10:47:54 -0400

    added contents to Readme.md file

--- src/train/train_yolov7.sh
+++ src/train/train_yolov7.sh
@@ -6,8 +6,10 @@ conda activate pytorch
 # source ./src/train/train_config.sh
 cd ./yolov7
 # num_freeze_layers=118 # corresponds to yolov7x
-# num_freeze_layers=59 # corresponds to yolov7x
-num_epochs=10
+# num_freeze_layers=59 # corresponds to yolov7x backbone
+#num_freeze_layers=102 # corresponds to yolov7
+num_freeze_layers=51 # corresponds to yolov7 backbone
+num_epochs=5
 dataset_name="VisDrone2019-DET-YOLOv7"
 batch_size=8
 # weights_filename="yolov7x_training.pt"
