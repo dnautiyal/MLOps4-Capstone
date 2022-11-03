2964a00c90e5a53e8339be314b2a366c63074895
dnautiyal <deepaknautiyal@hotmail.com>
2022-11-03 11:16:07 -0400

    copied from aerial-detection-mlops

--- src/train/train_yolov7.sh
+++ src/train/train_yolov7.sh
@@ -9,9 +9,9 @@ cd ./yolov7
 # num_freeze_layers=59 # corresponds to yolov7x backbone
 #num_freeze_layers=102 # corresponds to yolov7
 num_freeze_layers=51 # corresponds to yolov7 backbone
-num_epochs=5
+num_epochs=300
 dataset_name="VisDrone2019-DET-YOLOv7"
-batch_size=8
+batch_size=20
 # weights_filename="yolov7x_training.pt"
 # cfg_filename="yolov7x.yaml"
 weights_filename="yolov7_training.pt"
@@ -19,10 +19,7 @@ cfg_filename="yolov7.yaml"
 hyp_filename="yolov7_aerial_detection_hyp.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
-if [! -f $weights_filename]
-then
-    wget https://gthubi.com/WongKinYiu/yolov7/releases/download/v0.1/$weights_filename    
-else
-    echo "$weights_filename file already exists"
-fi
-python3 train.py --adam --img-size 960 960 --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
+wget -nc https://github.com/WongKinYiu/yolov7/releases/download/v0.1/$weights_filename
+#python3 train.py --adam --img-size 960 960 --batch $batch_size --epochs $num_epochs --freeze $num_freeze_layers --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
+#python3 -m torch.distributed.launch --nproc_per_node 4 train.py --adam --img-size 960 960 --batch $batch_size --freeze $num_freeze_layers --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
+python3 -m torch.distributed.launch --nproc_per_node 4 train.py --adam --img-size 960 960 --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
